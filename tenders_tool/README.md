9f8a01d75e7d92130d712931daf5b37dd9f7892a
PoivronJaune <poivronjaune@gmail.com>
2024-09-07 19:43:57 -0400

    pyproject dependencies added

---
+++ tenders_tool/README.md
@@ -0,0 +1,9 @@
+# TENDERS TOOL
+
+## Work in progress, alpha version (not production ready)
+
+Python package to analyse tenders information for government procurment contracts  
+Uses SEAO Open Data (Systeme Electronic d'Appel d'Offres) from the Province of Quebec in Canada  
+
+
+
--- tenders_tool/seao.py
+++ tenders_tool/seao.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from pandas import json_normalize
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+import copy
+import requests
+from datetime import datetime
+
 identifiant = 'd23b2e02-085d-43e5-9e6e-e1d558ebfdd5'
